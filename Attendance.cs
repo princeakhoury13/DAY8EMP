6ded25977805b30ee5f0d1c3aac9f71303ee72f6
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-17 10:16:28 +0530

    added: Check if employee is present

---
+++ Attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY8EMP
+{
+    public class Attendance
+    {
+        public void IsPresent()
+        {
+            int present = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
