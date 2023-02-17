cdf3791423f16f41020a720e971376dd195870fc
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-17 10:10:13 +0530

    added: Greetings message for homepage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DAY8EMP
+{
+    public class EMP
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee\r\nWage Computation\r\nProgram on Master Branch");
+        }
+    }
+}
